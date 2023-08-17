e9733936878f2ab732584bba63d70ab271a4b2de
Patrice Vignola <vignola.patrice@gmail.com>
2023-08-17 16:18:22 -0700

    Fix model conversion

--- update_kv_cache.py
+++ update_kv_cache.py
@@ -6,8 +6,8 @@ class UpdateKeyValueCache(torch.nn.Module):
         v_cache_out = torch.concat([v_cache, v_out], dim=2)
         return k_cache_out, v_cache_out
 
-pos = torch.tensor(0)
-seq_len = torch.tensor(1)
+pos = 0
+seq_len = 1
 
 k_cache = torch.randn(1, 32, pos, 32, 128, dtype=torch.float16)
 v_cache = torch.randn(1, 32, pos, 32, 128, dtype=torch.float16)
@@ -18,10 +18,10 @@ torch_model.eval()
 
 torch.onnx.export(
     torch_model,
-    (k_cache, v_cache, k_out, v_out, pos, seq_len),
+    (k_cache, v_cache, k_out, v_out),
     "update_kv_cache.onnx",
     do_constant_folding=True,
-    input_names = ["k_cache", "v_cache", "k_out", "v_out", "pos", "seq_len"],
+    input_names = ["k_cache", "v_cache", "k_out", "v_out"],
     output_names = ["k_cache_out", "v_cache_out"],
     dynamic_axes={
         "k_cache": {2: "pos"},
